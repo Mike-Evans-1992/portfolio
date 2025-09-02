08e7c82d0d7774ad831f386e87741a6bc35f8728
mevans2392 <evanscreatesapps@gmail.com>
2025-09-02 14:10:17 -0400

    start hero section and add placeholder for projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import avatar from './assets/avatar.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="flex flex-col md:flex-row w-screen h-screen items-center md:items-start justify-center md:justify-between md:p-8">
+        <div className="order-2 md:order-1 flex flex-col justify-center md:justify-between md:min-h-screen items-center">
+
+          <div className="md:pl-[100px] md:pt-[100px]">
+            <h1 className="text-4xl font-bold">Hello!</h1>
+            <h1 className="text-4xl font-bold">I'm Mike Evans</h1>
+            <h2 className="text-2xl font-semibold text-gray-600">Full Stack Developer</h2>
+          </div>
+          
+          <div className="mt-4 md:mb-10 md:pb-10 md:pl-[100px]">
+            <a href="#projects" className="md:inline-block min-w-[300px] text-center md:px-4 md:py-2 border-2 border-black text-black rounded hover:bg-green-600 transition">
+              View Projects
+            </a>
+          </div>
+
+        </div>
+
+        <div className="order-1 md:order-2 pb-[100px]">
+          <img src={avatar} alt="Avatar" className="rounded-full md:mb-4" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="flex flex-col w-screen h-screen items-center justify-start bg-gray-100" id="projects">
+        <h1 className="text-4xl font-bold pt-8">My Projects:</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </main>
     </>
   )
 }
